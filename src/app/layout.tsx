06fddaf9a40beab98dcf1933e0edc5f0a5def473
liemnt <liemdonduong@gmail.com>
2026-01-07 14:19:40 +0700

    chore: update gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,7 @@ const notoSans = localFont({
 export const metadata: Metadata = generateSEOMetadata();
 
 const GTM_ID = 'GTM-K8WZ86LZ';
+const GA_ID = 'G-RTQPPQ7GYM';
 
 export default function RootLayout({
     children,
@@ -74,6 +75,17 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`,
+                    }}
+                />
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_ID}');
+                        `,
                     }}
                 />
                 <link
